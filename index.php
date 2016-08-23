d52bcdb495ed166cafd2d3c73fad01d301958d7f
carpio08 <carina.muz@gmail.com>
2016-08-23 19:55:27 -0300

    primer commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+<head></head>
+<body>
+
+<?php
+	$miArray = array('1'=>90, '30'=>7, 'e'=>99, 'hola'=>"Mundo");
+
+	var_dump($miArray);
+	echo("<br>");
+	foreach ($miArray as $dato) {
+		echo($dato);
+		echo("<br>");
+	}
+?>
+
+
+</body>
+
+</html>
